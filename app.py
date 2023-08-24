8e2422ef56b180fc9fb0554231048a4ee0dfa18f
jjayakumar007 <134556299+jjayakumar007@users.noreply.github.com>
2023-08-25 01:30:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ app=Flask(__name__)
 def welcome():
     return "Welcome to flask"
 
-@app.route('/cal',method=['GET'])
+@app.route('/cal',methods=['GET'])
 def math_operator():
-    operation=request.jason['operation']
-    number1=request.jason['number1']
-    number2=request.jason['number2']
+    operation=request.json['operation']
+    number1=request.json['number1']
+    number2=request.json['number2']
 
-    if operation == "add":
+    if operation=="add":
         result=number1+number2
 
-    elif operation == "multuplication":
+    elif operation=="multuplication":
         result=number1*number2
 
-    elif operation == "division":
+    elif operation=="division":
         result=number1/number2
 
     else:
